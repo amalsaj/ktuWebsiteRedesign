1a8c67bb08ce777ba75b233cb8da167d5b4068a4
AMAL SAJI <103961626+amalsaj@users.noreply.github.com>
2024-09-21 13:52:33 +0530

    redesigned pages

--- src/components/Sign/Sign.js
+++ src/components/Sign/Sign.js
@@ -2,38 +2,33 @@ import React from "react";
 import { Container, Button, Card, Row, Col, Image } from "react-bootstrap";
 import logo from "../../Images/logo.png";
 import "./Sign.css";
+
 function Sign() {
   return (
-    <Container
-      fluid
-      className="d-flex justify-content-center align-content-center main1"
-    >
+    <Container fluid className="main1">
       <Row>
-        <Col className="logo-container d-flex m-3 text-black" lg={12}>
-          <Image
-            src={logo}
-            alt=""
-            width={42}
-            height={49}
-            rounded
-            className="mt-1"
-          />
-          <h1 className="ms-2 mt-2 fs-6  fw-bolder logo2">
-            APJ ABDUL KALAM <br></br>{" "}
-            <span className="text_head2">TECHNOLOGICAL UNIVERSITY</span>
-            <br></br>
-            <span className="text_head3">സാങ്കേതിക-ശാസ്ത്ര സർവകലാശാല</span>
-          </h1>
+        <Col className="logo-container">
+          <div className="logo-wrapper">
+            <Image src={logo} alt="Logo" className="logo-image" />
+            <div className="logo-text">
+              <h1 className="logo-title">APJ ABDUL KALAM</h1>
+              <h2 className="logo-subtitle">TECHNOLOGICAL UNIVERSITY</h2>
+              <h3 className="logo-subtitle-malayalam">
+                സാങ്കേതിക-ശാസ്ത്ര സർവകലാശാല
+              </h3>
+            </div>
+          </div>
         </Col>
       </Row>
+
       <Row>
-        <Col className="text-center" lg={12}>
+        <Col className="text-center">
           <Card className="card_sign">
             <Card.Header>
               <h1 className="fs-5 mt-2 heading fw-bolder">SIGN IN</h1>
             </Card.Header>
             <Card.Body>
-              <form autocomplete="off">
+              <form autoComplete="off">
                 <div className="mb-3">
                   <label htmlFor="username" className="form-label">
                     Username
@@ -56,15 +51,12 @@ function Sign() {
                     required
                   />
                 </div>
-
-                <div className="justify-content-center d-flex align-items-center mt-4">
-                  <Button type="submit" className="btn login">
-                    Login
-                  </Button>
-                </div>
-                <div className="justify-content-center d-flex align-items-center mt-4">
-                  <a href="/" className="text-decoration-none font">
-                    Forgot password
+                <Button type="submit" className="btn login">
+                  Login
+                </Button>
+                <div className="mt-2">
+                  <a href="/" className="text-decoration-none">
+                    Forgot password?
                   </a>
                 </div>
               </form>
